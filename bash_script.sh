cbd77bc6b93c6f0fefbdcf2da55f0d343c1e5c33
MiguelNarbot <92229321+MiguelNarbot@users.noreply.github.com>
2022-10-16 22:00:06 -0300

    Add files via upload

---
+++ bash_script.sh
@@ -0,0 +1,5 @@
+pwd                          #1) esse comando acaba exibindo o diretório do momento ou corrente. 
+ls -l /home                  #2) comando acaba por listar o conteúdo de um diretório
+ls -l /home > lista.HOME.txt #3) o comando ">" refere-se ao redirecionamento de entrada e saída de dados, portanto ele passa os dados obtidos para um outro arquivo, no caso ".txt"
+date                         #4) o comando expõe a data corrente no visor do utilitário
+
